8b7480c87db2e234185abf75181e25ddbb458360
your full name <email@seznam.cz>
2021-11-08 16:50:41 +0100

    Finished prototyping the asteroids, making them move will come in the next update

--- AlgProject/src/main/java/com/company/libgdx/entities/Asteroid.java
+++ AlgProject/src/main/java/com/company/libgdx/entities/Asteroid.java
@@ -24,6 +24,8 @@ public class Asteroid implements GameObject {
 
     float[] vertices;
 
+    private ShapeRenderer renderer = new ShapeRenderer();
+
     public Asteroid(String toParse) {
         toParse = toParse.substring(1, toParse.length() - 1);
         String[] points = toParse.split(", ");
@@ -31,46 +33,12 @@ public class Asteroid implements GameObject {
         for (int i = 0; i < points.length; i++) {
             vertices[i] = Float.parseFloat(points[i]);
         }
+        renderer.setAutoShapeType(true);
     }
-
-//    public Asteroid(float x, float y, GameScreen gameScreen){
-//        this.x = x;
-//        this.y = y;
-//        this.texture = new Texture("white.png");
-//        this.gameScreen = gameScreen;
-//        // this.body = BodyHelper.createBody(x, y, PLAYER_SIZE, PLAYER_SIZE, false
-//        //        , 10000, gameScreen.getWorld(), ContactType.PLAYER);
-//        Random rng = new Random();
-//
-//        int verticesCount = rng.nextInt(3) + 5;
-//        // int verticesCount = 6;
-//        final int maxLineLength = 30 * verticesCount / 2;
-//        this.vertices = new float[verticesCount * 2];
-//
-//        float radius = rng.nextFloat(30,80);
-//
-//        int startPointX = rng.nextInt(50,550);
-//        int startPointY = rng.nextInt(50,550);
-//
-//        for (int i = 1, j = 0; j < vertices.length; i++, j+=2) {
-//            float previousX = startPointX + (float) (Math.sin(i)*(radius / rng.nextFloat(1,3)));
-//            float previousY = startPointY + (float) (Math.cos(i)*(radius / rng.nextFloat(1,3)));
-//            vertices[j] = previousX;
-//            vertices[j + 1] = previousY;
-//        }
-//
-//        Polygon polygon = new Polygon(vertices);
-//        System.out.println(Arrays.toString(vertices));
-//    }
 
     @Override
     public void update() {
 
-    }
-
-    ShapeRenderer renderer = new ShapeRenderer();
-    {
-        renderer.setAutoShapeType(true);
     }
 
     @Override
--- Server/src/main/java/com/company/server_structure/room/entities/Asteroids.java
+++ Server/src/main/java/com/company/server_structure/room/entities/Asteroids.java
@@ -11,17 +11,28 @@ public class Asteroids {
     public static String createAsteroid() {
         int verticesCount = rng.nextInt(3) + 5;
 
-        final int maxLineLength = 30 * verticesCount / 2;
+        int startPointX = rng.nextInt(50,450);
+        int startPointY = rng.nextInt(50,450);
+
+        float maxRadius = rng.nextFloat(30,60);
+        float minRadius = maxRadius / 1.5f;
+
+
         float[] vertices = new float[verticesCount * 2];
 
-        float radius = rng.nextFloat(30,80);
+        float previousX;
+        float previousY;
 
-        int startPointX = rng.nextInt(50,550);
-        int startPointY = rng.nextInt(50,550);
-
+        int invertedPointChance = rng.nextInt(0, verticesCount * 2); // 50% chance for an inverted point
+        final int MAX_INVERTED_POINT_LENGTH = 8; // after a bit of testing, I came to the conclusion, that 8 is a good number
         for (int i = 1, j = 0; j < vertices.length; i++, j+=2) {
-            float previousX = startPointX + (float) (Math.sin(i) * (radius / rng.nextFloat(1,3)));
-            float previousY = startPointY + (float) (Math.cos(i) * (radius / rng.nextFloat(1,3)));
+            if(i == invertedPointChance) {
+                previousX = startPointX - (float) (Math.sin(i) * (rng.nextFloat(minRadius, maxRadius) / MAX_INVERTED_POINT_LENGTH));
+                previousY = startPointY - (float) (Math.cos(i) * (rng.nextFloat(minRadius, maxRadius) / MAX_INVERTED_POINT_LENGTH));
+            }else {
+                previousX = startPointX + (float) (Math.sin(i) * (rng.nextFloat(minRadius, maxRadius)));
+                previousY = startPointY + (float) (Math.cos(i) * (rng.nextFloat(minRadius, maxRadius)));
+            }
             vertices[j] = previousX;
             vertices[j + 1] = previousY;
         }
